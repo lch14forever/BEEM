6a5787d4b92b294f4b303f4bd5d07b8fd8a23db8
Chenhao Li <lch14forever@gmail.com>
2019-02-17 16:01:49 +0800

    update warning message

--- emFunctions.r
+++ emFunctions.r
@@ -624,7 +624,7 @@ EM <- function(dat, meta, forceBreak=NULL, useSpline=TRUE,
         stop("There are less than 7 species. This might results in an inaccurate model.")
     }    
     if(length(unique(meta$subjectID)) < 10){
-      warning("Less than 10 biological replicates detected. BEEM will assume that the samples are internally perturbed in each subject.")
+      warning("Small number (<10) of biological replicates detected. Note that BEEM works best with >10 biological replicates or the time series contains infrequent perturbations.")
     }
     refRank <- suggestRefs(dat, meta)
     if(is.null(refSp)){
